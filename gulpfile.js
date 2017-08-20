54da7dc202176f8b739129527b801889cbc0bb8f
raongithub <ramuasanongit@gmail.com>
2017-08-20 18:55:44 -0400

    Updated package.json & gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,16 @@
 var gulp = require ( 'gulp' );
+var watch = require( 'gulp-watch' );
+
+gulp.task( 'default', function () {
+  console.log( 'Created a Gulp task.' );
+});
+
+gulp.task( 'html', function () {
+  console.log( 'Do something useful being done to your HTML here.' );
+});
+
+gulp.task( 'watch', function () {
+  watch( './app/index.html', function () {
+    gulp.start( 'html' );
+  });
+});
